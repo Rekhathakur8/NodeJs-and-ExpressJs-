21683066c95d09d7dcb2bd6579a2a0b7243338b9
Rekha <rrekhathakur222@gmail.com>
2024-03-24 10:58:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/* there are two ways or method know as rapel and cli */
+// i am trying cli here
+
+const amount = 12;
+
+if (amount < 10) {
+  console.log("small number");
+} else {
+  console.log("large number");
+}
+
+console.log("it is my first code");
